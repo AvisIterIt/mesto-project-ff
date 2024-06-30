7b071b6f894d2e4b921d763337510aa6631ced16
AvisIterIt <artem.edjvin@gmail.com>
2024-06-30 16:12:42 +0500

    main

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,31 @@
 
 // @todo: Функция создания карточки
 
+function createCard(data) {
+  const cardTemplate = document.querySelector("#card-template").content;
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardTitle = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardTitle.textContent = data.name;
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+
+  deleteButton.addEventListener("click", function (e) {
+    e.target.closest(".card").remove();
+  });
+
+  return cardElement;
+}
+
+const cardList = document.querySelector(".places__list");
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData);
+  cardList.append(cardElement);
+});
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
